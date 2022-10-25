bd056a5abac2cc2ec005b22e0659e1662277d760
Zhenya-Trikoz <trikozzhenya@gmail.com>
2022-10-25 15:47:00 +0300

    Creating an application in fragments. Creating a test setup screen. Creating a toast screen with answer options.

---
+++ app/src/main/java/com/example/testenglish/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.testenglish;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.testenglish.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragmentContainerView, SettingFragment.newInstance())
+                    .commitNow();
+        }
+
+    }
+
+
+}
